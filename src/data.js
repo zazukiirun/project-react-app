f0f84ea7c5cb23c5149baef0a5c921c0ffc5edc4
zazukiirun <60461484+zazukiirun@users.noreply.github.com>
2021-08-29 00:40:00 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    id: 1,
+    title: 'MSI GE66 RAIDER',
+    price: 122880,
+    img:
+      'https://asset.msi.com/resize/image/global/product/product_1620192397e3979172ec9d8705381e5831cd809219.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'MSI ALPHA 15, AMD ADVANTAGE™ EDITION',
+    price: 25900,
+    img:
+      'https://asset.msi.com/resize/image/global/product/product_16239020881cca3507e788a29ec55a8a30fe35c41a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'MSI Katana GF76',
+    price: 48990,
+    img:
+      'https://asset.msi.com/resize/image/global/product/product_16190861921ff3e69f935cd3753b174a21f123c26f.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png',
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: 'MSI Creator 17',
+    price: 75990,
+    img:
+      'https://asset.msi.com/resize/image/global/product/product_4_20200323182459_5e788e7bc69fc.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png',
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: 'MSI GL65',
+    price: 35900,
+    img:
+      'https://asset.msi.com/resize/image/global/product/product_4_20190719105945_5d313221a3d89.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png',
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: 'MSI Stealth 15M',
+    price: 32590,
+    img:
+      'https://asset.msi.com/resize/image/global/product/product_16106060726b4598fc85cfa1269eba466fcfb06b85.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png',
+    amount: 1,
+    
+  },
+];
